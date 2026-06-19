c6ab3b0124b84b1e8f45068a6c28a01c72321540
umyunsang <164823608+umyunsang@users.noreply.github.com>
2026-06-19 20:36:57 +0900

    Update UMMAYA cask to v0.2.13

--- Casks/ummaya.rb
+++ Casks/ummaya.rb
@@ -3,9 +3,9 @@
 cask "ummaya" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.2.12"
-  sha256 arm:   "5dfc3dafa6337e492c707ccb5052c31eb908d4453663aac4a6a77dbe9cd8b28f",
-         intel: "26200d1804712ff36d3644845273dd33df10d873be9d9d52dee2c8334d3042bf"
+  version "0.2.13"
+  sha256 arm:   "f2b3d787a17f6707cc3a087bde9af85825a05054fdd4e26eddef1a70d6c832a7",
+         intel: "c675d1a58dd1c9bd82357e01c53b8bc93321470d327117b5e772745dab0e990f"
 
   url "https://ummaya-docs.pages.dev/downloads/homebrew/v#{version}/ummaya-#{version}-macos-#{arch}.tar.gz"
   name "UMMAYA"
